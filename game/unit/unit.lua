4c8c09c2cc5319059896ae7c9a7e5861e21071c5
Ben James <benjames171@gmail.com>
2024-05-05 18:25:34 +0100

    update

--- game/unit/unit.lua
+++ game/unit/unit.lua
@@ -4,7 +4,7 @@ M.data = {}
 M.names = {"TROOP", "MECH", "SCOUT"}
 M.arm = {6,8,4}
 M.pow = {4,6,2}
-M.movmax = {6,4,8}
+M.movemax = {6,4,8}
 M.firemax = {2,1,3}
 
 function M.init()
@@ -12,15 +12,15 @@ function M.init()
 end
 
 function M.add(x, y, team, type, url)
-	local unit = {	x = x, y = y, team = team, type = type, hp = 8, url = url,
-					arm = M.arm[type], pow = M.pow[type], movmax = M.movmax[type], firemax = M.firemax[type]}
+	local unit = {	x = x, y = y, team = team, type = type, hp = 8, url = url, arm = M.arm[type], pow = M.pow[type],
+		move = M.movemax[type], movmax = M.movemax[type], fire = M.firemax[type], firemax = M.firemax[type]}
 	table.insert(M.data, unit)
 end
 
 function M.prepteam(team)
 	for _, v in pairs(M.data) do
 		if v.team == team then
-			v.mov = v.movmax
+			v.move = v.movemax
 			v.fire = v.firemax
 		end
 	end
