c08f2bc58a6b4da44218d1d23ac32b358e37bb41
Ben James <benjames171@gmail.com>
2024-05-09 13:50:15 +0100

    update

---
+++ game/unit/shot.go
@@ -0,0 +1,38 @@
+components {
+  id: "script"
+  component: "/game/unit/shot.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"shot\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/game/core/16.tilesource\"\n"
+  "}\n"
+  ""
+  position {
+    x: -8.0
+    y: -8.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
