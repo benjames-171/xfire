a310cd3fd4520e279a9e3d37ec98b9eba88e8542
Ben James <benjames171@gmail.com>
2024-05-16 21:50:00 +0100

    update

--- game/unit/unit.go
+++ game/unit/unit.go
@@ -70,3 +70,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "regen"
+  type: "sprite"
+  data: "default_animation: \"regen\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: -8.0
+    y: 6.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
